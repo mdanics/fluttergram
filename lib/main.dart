f75513f18cc556cadb158bb0b6233a4ce6af2ad3
Matthew <matthew.danics@tapmango.com>
2018-03-17 10:09:55 +0200

    added firebase auth + login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,43 @@
 import 'package:flutter/material.dart';
 import 'feed.dart';
 import 'upload_page.dart';
+import 'dart:async';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final auth = FirebaseAuth.instance;
+final googleSignIn = new GoogleSignIn();
+
+Future<Null> _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null) {
+    user = await googleSignIn.signInSilently();
+  }
+  if (user == null) {
+    await googleSignIn.signIn();
+  }
+
+  if (await auth.currentUser() == null) {
+    GoogleSignInAuthentication credentials =
+        await googleSignIn.currentUser.authentication;
+    await auth.signInWithGoogle(
+        idToken: credentials.idToken, accessToken: credentials.accessToken);
+  }
+}
+
+Future<Null> _silentLogin() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null) {
+    user = await googleSignIn.signInSilently();
+  }
+
+  if (await auth.currentUser() == null && user != null) {
+    GoogleSignInAuthentication credentials =
+        await googleSignIn.currentUser.authentication;
+    await auth.signInWithGoogle(
+        idToken: credentials.idToken, accessToken: credentials.accessToken);
+  }
+}
 
 void main() => runApp(new MyApp());
 
@@ -21,7 +58,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new HomePage(title: 'Flutter Demo Home Page'),
+      home: new HomePage(title: 'Fluttergram'),
     );
   }
 }
@@ -38,52 +75,82 @@ class _HomePageState extends State<HomePage> {
   PageController _pageController;
 
   int _page = 0;
+  bool triedSilentLogin = false;
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new PageView(
-        children: [
-          new Container(
-            color: Colors.white,
-            child: new Feed(),
-          ),
-          new Container(color: Colors.green,),
-          new Container(color: Colors.white, child: new Uploader()),
-          new Container(color: Colors.amber),
-          new Container(color: Colors.white, child: new PostForm()),
-        ],
-        controller: _pageController,
-        physics: new NeverScrollableScrollPhysics(),
-        onPageChanged: onPageChanged,
-      ),
-      bottomNavigationBar: new BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          new BottomNavigationBarItem(
-              icon: new Icon(Icons.home, color: Colors.grey),
-              title: new Text(""),
-              backgroundColor: Colors.white),
-          new BottomNavigationBarItem(
-              icon: new Icon(Icons.search, color: Colors.grey),
-              title: new Text(""),
-              backgroundColor: Colors.white),
-          new BottomNavigationBarItem(
-              icon: new Icon(Icons.add_circle, color: Colors.grey),
-              title: new Text(""),
-              backgroundColor: Colors.white),
-          new BottomNavigationBarItem(
-              icon: new Icon(Icons.star, color: Colors.grey),
-              title: new Text(""),
-              backgroundColor: Colors.white),
-          new BottomNavigationBarItem(
-              icon: new Icon(Icons.person_outline, color: Colors.grey),
-              title: new Text(""),
-              backgroundColor: Colors.white),
-        ],
-        onTap: navigationTapped,
-        currentIndex: _page,
-      ),
-    );
+    if (triedSilentLogin == false) {
+      silentLogin();
+    } // might cause performance issues?
+    return googleSignIn.currentUser == null
+        ? new Container(
+            alignment: FractionalOffset.center,
+            width: 20.0,
+            child: new RaisedButton(
+              onPressed: login,
+              child: new Row(children: <Widget>[
+                new Icon(Icons.business),
+                new Text("Sign in with Google")
+              ]),
+            ),
+          )
+        : new Scaffold(
+            body: new PageView(
+              children: [
+                new Container(
+                  color: Colors.white,
+                  child: new Feed(),
+                ),
+                new Container(
+                  color: Colors.green,
+                ),
+                new Container(color: Colors.white, child: new Uploader()),
+                new Container(color: Colors.amber),
+                new Container(color: Colors.white, child: new Text('Test')),
+              ],
+              controller: _pageController,
+              physics: new NeverScrollableScrollPhysics(),
+              onPageChanged: onPageChanged,
+            ),
+            bottomNavigationBar: new BottomNavigationBar(
+              items: <BottomNavigationBarItem>[
+                new BottomNavigationBarItem(
+                    icon: new Icon(Icons.home, color: Colors.grey),
+                    title: new Text(""),
+                    backgroundColor: Colors.white),
+                new BottomNavigationBarItem(
+                    icon: new Icon(Icons.search, color: Colors.grey),
+                    title: new Text(""),
+                    backgroundColor: Colors.white),
+                new BottomNavigationBarItem(
+                    icon: new Icon(Icons.add_circle, color: Colors.grey),
+                    title: new Text(""),
+                    backgroundColor: Colors.white),
+                new BottomNavigationBarItem(
+                    icon: new Icon(Icons.star, color: Colors.grey),
+                    title: new Text(""),
+                    backgroundColor: Colors.white),
+                new BottomNavigationBarItem(
+                    icon: new Icon(Icons.person_outline, color: Colors.grey),
+                    title: new Text(""),
+                    backgroundColor: Colors.white),
+              ],
+              onTap: navigationTapped,
+              currentIndex: _page,
+            ),
+          );
+  }
+
+  void login() async {
+    await _ensureLoggedIn();
+    setState(() {
+      triedSilentLogin = true;
+    });
+  }
+
+  void silentLogin() async {
+    await _silentLogin();
+    setState(() {});
   }
 
   void navigationTapped(int page) {
