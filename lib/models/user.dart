b0aff0e2f19d546396126b8fea4b2b115bc04a8b
Matthew <matthewdanics@gmail.com>
2019-05-02 14:41:03 -0400

    separate user model to its own file

---
+++ lib/models/user.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String email;
+  final String id;
+  final String photoUrl;
+  final String username;
+  final String displayName;
+  final String bio;
+  final Map followers;
+  final Map following;
+
+  const User(
+      {this.username,
+      this.id,
+      this.photoUrl,
+      this.email,
+      this.displayName,
+      this.bio,
+      this.followers,
+      this.following});
+
+  factory User.fromDocument(DocumentSnapshot document) {
+    return new User(
+      email: document['email'],
+      username: document['username'],
+      photoUrl: document['photoUrl'],
+      id: document.documentID,
+      displayName: document['displayName'],
+      bio: document['bio'],
+      followers: document['followers'],
+      following: document['following'],
+    );
+  }
+}
